46a6f185c004faae1775ef92dcdd4cf73ac509ec
Rene Hexel <rhx@users.noreply.github.com>
2023-01-20 05:59:32 +1000

    Initial import

---
+++ Tests/ScriptingTests/ScriptingTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Scripting
+
+final class ScriptingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Scripting().text, "Hello, World!")
+    }
+}
