2bb7ec19d17f6def7664f7914c942131f3961b30
Rene Hexel <r.hexel@griffith.edu.au>
2023-01-27 10:29:31 +1000

    Publish to github pages

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
